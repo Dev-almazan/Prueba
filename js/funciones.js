36872b11138cfce89c613acb00ff35504f304f3a
Ernesto Almazán Reséndiz <73855556+Dev-almazan@users.noreply.github.com>
2021-03-22 12:16:38 -0600

    Iniciando prueba

---
+++ js/funciones.js
@@ -0,0 +1,73 @@
+
+ // CADA TARJETA SE CLONA Y SE MUESTRA EN EL EL DIV  PARA HACER EL EFECTO FIXED Y PODERLO OCULTAR PARA LAS MEDIAS QUERYS
+ 
+$(document).ready(function(){
+  
+$("#img1").hover(function () {
+ $('#card:first').clone(true).appendTo('.ca').off;
+  $('.ca').toggle();
+    return true;
+});
+     
+$("#img2").hover(function () {
+ $('#cardb:first').clone(true).appendTo('.cb').off;
+  $('.cb').toggle();
+    return true;
+});     
+         
+     
+$("#img3").hover(function () {
+ $('#cardc:first').clone(true).appendTo('.cc').off;
+  $('.cc').toggle();
+    return true;
+});     
+     
+$("#img4").hover(function () {
+ $('#cardd:first').clone(true).appendTo('.cd').off;
+  $('.cd').toggle();
+    return true;
+});
+     
+$("#img5").hover(function () {
+ $('#carde:first').clone(true).appendTo('.ce').off;
+  $('.ce').toggle();
+    return true;
+});
+     
+$("#img6").hover(function () {
+ $('#cardf:first').clone(true).appendTo('.cf').off;
+  $('.cf').toggle();
+    return true;
+});     
+         
+     
+$("#img7").hover(function () {
+ $('#cardg:first').clone(true).appendTo('.cg').off;
+  $('.cg').toggle();
+    return true;
+});     
+     
+$("#img8").hover(function () {
+ $('#cardh:first').clone(true).appendTo('.ch').off;
+  $('.ch').toggle();
+    return true;
+});
+       
+     
+$("#img9").hover(function () {
+ $('#cardi:first').clone(true).appendTo('.ci').off;
+  $('.ci').toggle();
+    return true;
+});
+      
+  $("#img10").hover(function () {
+ $('#cardj:first').clone(true).appendTo('.cj').off;
+  $('.cj').toggle();
+    return true;
+});
+         
+    
+});
+
+       
+     
